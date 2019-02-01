f8c0f044e5c60887b3bda8dd7c4b3377a8aa54f6
z14git <lzl1992@gmail.com>
2019-02-01 18:21:52 +0800

    [spi] 修改spi1引脚，以适配PCB

--- bsp/stm32f4xx-HAL/drivers/drv_spi.c
+++ bsp/stm32f4xx-HAL/drivers/drv_spi.c
@@ -309,18 +309,18 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef *spiHandle)
     {
         /* SPI1 clock enable */
         __HAL_RCC_SPI1_CLK_ENABLE();
-        __HAL_RCC_GPIOA_CLK_ENABLE();
-        /**SPI1 GPIO Configuration
-        PA5     ------> SPI1_SCK
-        PA6     ------> SPI1_MISO
-        PA7     ------> SPI1_MOSI
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        /**SPI1 GPIO Configuration    
+        PB3     ------> SPI1_SCK
+        PB4     ------> SPI1_MISO
+        PB5     ------> SPI1_MOSI 
         */
-        GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
+        GPIO_InitStruct.Pin = GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
         GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
     }
     else if (spiHandle->Instance == SPI2)
     {
