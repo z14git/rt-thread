9ba18944b02441d60116f720988be1273f346d3a
z14git <lzl1992@gmail.com>
2019-09-19 16:08:46 +0800

    添加eeprom读写函数到cpp环境中

---
+++ bsp/stm32/stm32f103-atk-nano/applications/bsp_at24cxx.h
@@ -0,0 +1,25 @@
+/**
+ * @file bsp_at24cxx.h
+ * @author z14git
+ * @brief 
+ * @version 0.1
+ * @date 2019-09-19
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#ifndef __BSP_AT24CXX_H 
+#define __BSP_AT24CXX_H 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <rtthread.h>
+rt_err_t at24cxx_write_byte(rt_uint8_t wr_addr, rt_uint8_t data);
+rt_err_t at24cxx_read_byte(rt_uint8_t rd_addr, rt_uint8_t *rd_buf);
+#ifdef __cplusplus
+}
+#endif
+
+#endif	// __BSP_AT24CXX_H
