538fb9252f9d286c4be50eee75a35284985b23bb
z14git <lzl1992@gmail.com>
2019-11-13 18:09:07 +0800

    启用adc功能

--- bsp/stm32/stm32f103-atk-nano/board/board.c
+++ bsp/stm32/stm32f103-atk-nano/board/board.c
@@ -42,7 +42,8 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC|RCC_PERIPHCLK_USB;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
   PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
