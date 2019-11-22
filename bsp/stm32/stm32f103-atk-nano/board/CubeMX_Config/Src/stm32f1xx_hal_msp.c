5c0f197ba857ac7359a23c8e81e8bca142dc0880
z14git <lzl1992@gmail.com>
2019-11-22 17:22:46 +0800

    开启pwm2 ch1

--- bsp/stm32/stm32f103-atk-nano/board/CubeMX_Config/Src/stm32f1xx_hal_msp.c
+++ bsp/stm32/stm32f103-atk-nano/board/CubeMX_Config/Src/stm32f1xx_hal_msp.c
@@ -270,9 +270,10 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
   
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /**TIM2 GPIO Configuration    
+    PA0-WKUP     ------> TIM2_CH1
     PA1     ------> TIM2_CH2 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
--- bsp/stm32/stm32f103-atk-nano/rtconfig.h
+++ bsp/stm32/stm32f103-atk-nano/rtconfig.h
@@ -262,6 +262,7 @@
 #define BSP_USING_TIM4
 #define BSP_USING_PWM
 #define BSP_USING_PWM2
+#define BSP_USING_PWM2_CH1
 #define BSP_USING_PWM2_CH2
 #define BSP_USING_SPI
 #define BSP_USING_SPI1
