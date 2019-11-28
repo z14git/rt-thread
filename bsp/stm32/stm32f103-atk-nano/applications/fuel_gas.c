87ec1063e6d9f9b4f7c849f0e159da2c6a89b070
z14git <lzl1992@gmail.com>
2019-11-28 17:03:04 +0800

    [燃气传感器] 加数据点读取功能

--- bsp/stm32/stm32f103-atk-nano/applications/fuel_gas.c
+++ bsp/stm32/stm32f103-atk-nano/applications/fuel_gas.c
@@ -38,10 +38,16 @@ static int fuel_gas_read(void *cmd, void *data)
 {
     uint16_t value;
     value = get_adc(FUEL_GAS_SENSOR_CHANNEL);
+    if ((uint32_t)cmd != 0) {
+        if (rt_strcmp((char *)cmd, "adc") == 0) {
+            *(double *)data = value;
+            return 0;
+        }
+    } else {
+        rt_snprintf(buf, 12, "%d", value);
 
-    rt_snprintf(buf, 12, "%d", value);
-
-    *(char **)data = buf;
+        *(char **)data = buf;
+    }
     return 0;
 }
 
