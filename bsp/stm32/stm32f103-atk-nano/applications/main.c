662cba41344f51620439fe463497b39ef7082fcd
z14git <lzl1992@gmail.com>
2020-01-09 09:29:42 +0800

    删除多余的DB9测试代码

--- bsp/stm32/stm32f103-atk-nano/applications/main.c
+++ bsp/stm32/stm32f103-atk-nano/applications/main.c
@@ -48,11 +48,6 @@ int main(void)
         rt_pin_write(LED0_PIN, PIN_LOW);
         rt_pin_write(LED1_PIN, PIN_LOW);
         rt_thread_mdelay(500);
-
-        /* 每3秒发送一次'hola'，用于DB9测试 */
-        if (count % 3 == 0) {
-            rt_kprintf("hola\n");
-        }
     }
 
     return RT_EOK;
