2b6c4ca36018167c626d34b03b40945ea6bdb9c8
z14git <lzl1992@gmail.com>
2020-02-12 15:14:22 +0800

    update .config

--- bsp/stm32/stm32f103-atk-nano/rtconfig.h
+++ bsp/stm32/stm32f103-atk-nano/rtconfig.h
@@ -40,7 +40,7 @@
 #define RT_USING_CONSOLE
 #define RT_CONSOLEBUF_SIZE 128
 #define RT_CONSOLE_DEVICE_NAME "uart1"
-#define RT_VER_NUM 0x40002
+#define RT_VER_NUM 0x40003
 #define ARCH_ARM
 #define RT_USING_CPU_FFS
 #define ARCH_ARM_CORTEX_M
@@ -98,6 +98,7 @@
 #define RT_USING_SFUD
 #define RT_SFUD_USING_SFDP
 #define RT_SFUD_USING_FLASH_INFO_TABLE
+#define RT_SFUD_SPI_MAX_HZ 50000000
 
 /* Using USB */
 
