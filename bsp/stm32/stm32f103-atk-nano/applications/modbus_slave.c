3abda4d484d440cce72db802c20252845d49df2f
z14git <lzl1992@gmail.com>
2020-06-29 14:31:21 +0800

    [modbus_slave] 改为使用libmodbus

--- bsp/stm32/stm32f103-atk-nano/applications/modbus_slave.c
+++ bsp/stm32/stm32f103-atk-nano/applications/modbus_slave.c
@@ -13,8 +13,8 @@
 #include <rtdevice.h>
 #include <board.h>
 
-#include "mb.h"
-#include "user_mb_app.h"
+#include "modbus.h"
+
 #include "servo_process.h"
 
 #ifndef ULOG_USING_SYSLOG
@@ -25,11 +25,10 @@
 #include <syslog.h>
 #endif /* ULOG_USING_SYSLOG */
 
-#define SLAVE_ADDR 14
-#define PORT_NUM 2
-#define PORT_BAUDRATE 115200
+#define MODBUS_DEVICE "/dev/uart2"
 
-#define PORT_PARITY MB_PAR_NONE
+#define SLAVE_ADDR 14
+#define PORT_BAUDRATE 115200
 
 #define THREAD_STACK_SIZE 2048
 #define THRAD_PRIORITY 15
@@ -57,290 +56,89 @@ typedef enum {
         ADAPTER_MODBUS_BIT_MAX
 } adapter_modbus_bit_t;
 
-/*------------------------Slave mode use these variables----------------------*/
-//Slave mode:DiscreteInputs variables
-USHORT usSDiscInStart = S_DISCRETE_INPUT_START;
-#if S_DISCRETE_INPUT_NDISCRETES % 8
-UCHAR ucSDiscInBuf[S_DISCRETE_INPUT_NDISCRETES / 8 + 1];
-#else
-UCHAR ucSDiscInBuf[S_DISCRETE_INPUT_NDISCRETES / 8];
-#endif
-//Slave mode:Coils variables
-USHORT usSCoilStart = S_COIL_START;
-#if S_COIL_NCOILS % 8
-UCHAR ucSCoilBuf[S_COIL_NCOILS / 8 + 1];
-#else
-UCHAR ucSCoilBuf[S_COIL_NCOILS / 8];
-#endif
-//Slave mode:InputRegister variables
-USHORT usSRegInStart = S_REG_INPUT_START;
-USHORT usSRegInBuf[S_REG_INPUT_NREGS];
-//Slave mode:HoldingRegister variables
-USHORT usSRegHoldStart = S_REG_HOLDING_START;
-USHORT usSRegHoldBuf[S_REG_HOLDING_NREGS];
+static modbus_mapping_t *mb_mapping = NULL;
 
-static void hold_reg_process(uint16_t index, uint16_t data)
+/**
+ * @brief 用于保存上一次的HOLD REG值，以便检查哪些HOLD REG的值更新了
+ * 
+ */
+static uint8_t modbus_config_buffer[ADAPTER_MODBUS_CONFIG_MAX];
+
+/**
+ * @brief 检测哪些HOLD REG 或 coil 被修改，根据其改变，发送相应的信息给其他线程处理
+ * 
+ * @param old_state 保存上一次HOLD REG状态的数组
+ * @param mb_mapping 
+ */
+static void hold_reg_process(uint8_t *old_state,
+                                 modbus_mapping_t *mb_mapping)
 {
         struct adapter_servo_msg servo_msg;
-        switch (index) {
-        case ADAPTER_MODBUS_CONFIG_UP_SERVO_ANGLE:
+
+#define IS_HOLD_REG_UPDATED(x)                                                 \
+        !(old_state[x] == (mb_mapping->tab_registers[x] & 0xff))
+
+#define SAVE_HOLD_REG(x)                                                       \
+        do {                                                                   \
+                old_state[x] = mb_mapping->tab_registers[x];                   \
+        } while (0)
+
+        /* handle servo */
+        if (IS_HOLD_REG_UPDATED(ADAPTER_MODBUS_CONFIG_SERVO_ON)) {
+                servo_msg.cmd = ADAPTER_SERVO_CMD_ON_OFF;
+                servo_msg.data =
+                        mb_mapping
+                                ->tab_registers[ADAPTER_MODBUS_CONFIG_SERVO_ON];
+                adapter_servo_msg_put(&servo_msg);
+                SAVE_HOLD_REG(ADAPTER_MODBUS_CONFIG_SERVO_ON);
+        }
+
+        if (IS_HOLD_REG_UPDATED(ADAPTER_MODBUS_CONFIG_UP_SERVO_ANGLE)) {
                 servo_msg.cmd = ADAPTER_SERVO_CMD_UP_ANGLE;
-                servo_msg.data = data;
+                servo_msg.data = mb_mapping->tab_registers
+                                   [ADAPTER_MODBUS_CONFIG_UP_SERVO_ANGLE];
                 adapter_servo_msg_put(&servo_msg);
-                break;
-        case ADAPTER_MODBUS_CONFIG_DOWN_SERVO_ANGLE:
+                SAVE_HOLD_REG(ADAPTER_MODBUS_CONFIG_UP_SERVO_ANGLE);
+        }
+
+        if (IS_HOLD_REG_UPDATED(ADAPTER_MODBUS_CONFIG_DOWN_SERVO_ANGLE)) {
                 servo_msg.cmd = ADAPTER_SERVO_CMD_DOWN_ANGLE;
-                servo_msg.data = data;
+                servo_msg.data = mb_mapping->tab_registers
+                                   [ADAPTER_MODBUS_CONFIG_DOWN_SERVO_ANGLE];
                 adapter_servo_msg_put(&servo_msg);
-                break;
-        default:
-                break;
+                SAVE_HOLD_REG(ADAPTER_MODBUS_CONFIG_DOWN_SERVO_ANGLE);
         }
-}
-
-/**
- * Modbus slave input register callback function.
- *
- * @param pucRegBuffer input register buffer
- * @param usAddress input register address
- * @param usNRegs input register number
- *
- * @return result
- */
-eMBErrorCode eMBRegInputCB(UCHAR *pucRegBuffer, USHORT usAddress,
-                           USHORT usNRegs)
-{
-        eMBErrorCode eStatus = MB_ENOERR;
-        USHORT iRegIndex;
-        USHORT *pusRegInputBuf;
-        USHORT REG_INPUT_START;
-        USHORT REG_INPUT_NREGS;
-        USHORT usRegInStart;
-
-        pusRegInputBuf = usSRegInBuf;
-        REG_INPUT_START = S_REG_INPUT_START;
-        REG_INPUT_NREGS = S_REG_INPUT_NREGS;
-        usRegInStart = usSRegInStart;
-
-        /* it already plus one in modbus function method. */
-        usAddress--;
-
-        if ((usAddress >= REG_INPUT_START) &&
-            (usAddress + usNRegs <= REG_INPUT_START + REG_INPUT_NREGS)) {
-                iRegIndex = usAddress - usRegInStart;
-                while (usNRegs > 0) {
-                        *pucRegBuffer++ =
-                                (UCHAR)(pusRegInputBuf[iRegIndex] >> 8);
-                        *pucRegBuffer++ =
-                                (UCHAR)(pusRegInputBuf[iRegIndex] & 0xFF);
-                        iRegIndex++;
-                        usNRegs--;
-                }
-        } else {
-                eStatus = MB_ENOREG;
-        }
-
-        return eStatus;
-}
-
-/**
- * Modbus slave holding register callback function.
- *
- * @param pucRegBuffer holding register buffer
- * @param usAddress holding register address
- * @param usNRegs holding register number
- * @param eMode read or write
- *
- * @return result
- */
-eMBErrorCode eMBRegHoldingCB(UCHAR *pucRegBuffer, USHORT usAddress,
-                             USHORT usNRegs, eMBRegisterMode eMode)
-{
-        eMBErrorCode eStatus = MB_ENOERR;
-        USHORT iRegIndex;
-        USHORT *pusRegHoldingBuf;
-        USHORT REG_HOLDING_START;
-        USHORT REG_HOLDING_NREGS;
-        USHORT usRegHoldStart;
-
-        pusRegHoldingBuf = usSRegHoldBuf;
-        REG_HOLDING_START = S_REG_HOLDING_START;
-        REG_HOLDING_NREGS = S_REG_HOLDING_NREGS;
-        usRegHoldStart = usSRegHoldStart;
-
-        /* it already plus one in modbus function method. */
-        usAddress--;
-
-        if ((usAddress >= REG_HOLDING_START) &&
-            (usAddress + usNRegs <= REG_HOLDING_START + REG_HOLDING_NREGS)) {
-                iRegIndex = usAddress - usRegHoldStart;
-                switch (eMode) {
-                /* read current register values from the protocol stack. */
-                case MB_REG_READ:
-                        while (usNRegs > 0) {
-                                *pucRegBuffer++ = (UCHAR)(
-                                        pusRegHoldingBuf[iRegIndex] >> 8);
-                                *pucRegBuffer++ = (UCHAR)(
-                                        pusRegHoldingBuf[iRegIndex] & 0xFF);
-                                iRegIndex++;
-                                usNRegs--;
-                        }
-                        break;
-
-                /* write current register values with new values from the protocol stack. */
-                case MB_REG_WRITE:
-                        while (usNRegs > 0) {
-                                pusRegHoldingBuf[iRegIndex] = *pucRegBuffer++
-                                                              << 8;
-                                pusRegHoldingBuf[iRegIndex] |= *pucRegBuffer++;
-                                hold_reg_process(iRegIndex,
-                                                 pusRegHoldingBuf[iRegIndex]);
-                                LOG_D("MB_REG_WRITE INDEX: %d, data: %d",
-                                      iRegIndex, pusRegHoldingBuf[iRegIndex]);
-                                iRegIndex++;
-                                usNRegs--;
-                        }
-                        break;
-                }
-        } else {
-                eStatus = MB_ENOREG;
-        }
-        return eStatus;
-}
-
-/**
- * Modbus slave coils callback function.
- *
- * @param pucRegBuffer coils buffer
- * @param usAddress coils address
- * @param usNCoils coils number
- * @param eMode read or write
- *
- * @return result
- */
-eMBErrorCode eMBRegCoilsCB(UCHAR *pucRegBuffer, USHORT usAddress,
-                           USHORT usNCoils, eMBRegisterMode eMode)
-{
-        eMBErrorCode eStatus = MB_ENOERR;
-        USHORT iRegIndex, iRegBitIndex, iNReg;
-        UCHAR *pucCoilBuf;
-        USHORT COIL_START;
-        USHORT COIL_NCOILS;
-        USHORT usCoilStart;
-        iNReg = usNCoils / 8 + 1;
-
-        pucCoilBuf = ucSCoilBuf;
-        COIL_START = S_COIL_START;
-        COIL_NCOILS = S_COIL_NCOILS;
-        usCoilStart = usSCoilStart;
-
-        /* it already plus one in modbus function method. */
-        usAddress--;
-
-        if ((usAddress >= COIL_START) &&
-            (usAddress + usNCoils <= COIL_START + COIL_NCOILS)) {
-                iRegIndex = (USHORT)(usAddress - usCoilStart) / 8;
-                iRegBitIndex = (USHORT)(usAddress - usCoilStart) % 8;
-                switch (eMode) {
-                /* read current coil values from the protocol stack. */
-                case MB_REG_READ:
-                        while (iNReg > 0) {
-                                *pucRegBuffer++ =
-                                        xMBUtilGetBits(&pucCoilBuf[iRegIndex++],
-                                                       iRegBitIndex, 8);
-                                iNReg--;
-                        }
-                        pucRegBuffer--;
-                        /* last coils */
-                        usNCoils = usNCoils % 8;
-                        /* filling zero to high bit */
-                        *pucRegBuffer = *pucRegBuffer << (8 - usNCoils);
-                        *pucRegBuffer = *pucRegBuffer >> (8 - usNCoils);
-                        break;
-
-                        /* write current coil values with new values from the protocol stack. */
-                case MB_REG_WRITE:
-                        while (iNReg > 1) {
-                                xMBUtilSetBits(&pucCoilBuf[iRegIndex++],
-                                               iRegBitIndex, 8,
-                                               *pucRegBuffer++);
-                                iNReg--;
-                        }
-                        /* last coils */
-                        usNCoils = usNCoils % 8;
-                        /* xMBUtilSetBits has bug when ucNBits is zero */
-                        if (usNCoils != 0) {
-                                xMBUtilSetBits(&pucCoilBuf[iRegIndex++],
-                                               iRegBitIndex, usNCoils,
-                                               *pucRegBuffer++);
-                        }
-                        break;
-                }
-        } else {
-                eStatus = MB_ENOREG;
-        }
-        return eStatus;
-}
-
-/**
- * Modbus slave discrete callback function.
- *
- * @param pucRegBuffer discrete buffer
- * @param usAddress discrete address
- * @param usNDiscrete discrete number
- *
- * @return result
- */
-eMBErrorCode eMBRegDiscreteCB(UCHAR *pucRegBuffer, USHORT usAddress,
-                              USHORT usNDiscrete)
-{
-        eMBErrorCode eStatus = MB_ENOERR;
-        USHORT iRegIndex, iRegBitIndex, iNReg;
-        UCHAR *pucDiscreteInputBuf;
-        USHORT DISCRETE_INPUT_START;
-        USHORT DISCRETE_INPUT_NDISCRETES;
-        USHORT usDiscreteInputStart;
-        iNReg = usNDiscrete / 8 + 1;
-
-        pucDiscreteInputBuf = ucSDiscInBuf;
-        DISCRETE_INPUT_START = S_DISCRETE_INPUT_START;
-        DISCRETE_INPUT_NDISCRETES = S_DISCRETE_INPUT_NDISCRETES;
-        usDiscreteInputStart = usSDiscInStart;
-
-        /* it already plus one in modbus function method. */
-        usAddress--;
-
-        if ((usAddress >= DISCRETE_INPUT_START) &&
-            (usAddress + usNDiscrete <=
-             DISCRETE_INPUT_START + DISCRETE_INPUT_NDISCRETES)) {
-                iRegIndex = (USHORT)(usAddress - usDiscreteInputStart) / 8;
-                iRegBitIndex = (USHORT)(usAddress - usDiscreteInputStart) % 8;
-
-                while (iNReg > 0) {
-                        *pucRegBuffer++ = xMBUtilGetBits(
-                                &pucDiscreteInputBuf[iRegIndex++], iRegBitIndex,
-                                8);
-                        iNReg--;
-                }
-                pucRegBuffer--;
-                /* last discrete */
-                usNDiscrete = usNDiscrete % 8;
-                /* filling zero to high bit */
-                *pucRegBuffer = *pucRegBuffer << (8 - usNDiscrete);
-                *pucRegBuffer = *pucRegBuffer >> (8 - usNDiscrete);
-        } else {
-                eStatus = MB_ENOREG;
-        }
-
-        return eStatus;
 }
 
 static void mb_slave_handler(void *arg)
 {
-        eMBInit(MB_RTU, SLAVE_ADDR, PORT_NUM, PORT_BAUDRATE, PORT_PARITY);
-        eMBEnable();
-        for (;;) {
-                eMBPoll();
+        int rc;
+        uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH];
+        modbus_t *ctx = RT_NULL;
+        ctx = modbus_new_rtu(MODBUS_DEVICE, 115200, 'N', 8, 1);
+        modbus_rtu_set_serial_mode(ctx, MODBUS_RTU_RS232);
+        modbus_set_slave(ctx, SLAVE_ADDR);
+        modbus_connect(ctx);
+        modbus_set_response_timeout(ctx, 0, 1000000);
+        mb_mapping = modbus_mapping_new(ADAPTER_MODBUS_BIT_MAX + 1, 0,
+                                        ADAPTER_MODBUS_CONFIG_MAX + 1, 0);
+        if (mb_mapping == NULL) {
+                modbus_free(ctx);
+                return;
+        }
+
+        mb_mapping->tab_bits[ADAPTER_MODBUS_BIT_LEFT_BLOCK] = 0;
+        mb_mapping->tab_bits[ADAPTER_MODBUS_BIT_LEFT_PASS] = 0;
+        mb_mapping->tab_bits[ADAPTER_MODBUS_BIT_RIGHT_BLOCK] = 0;
+        mb_mapping->tab_bits[ADAPTER_MODBUS_BIT_RIGHT_PASS] = 0;
+
+        while (1) {
+                rc = modbus_receive(ctx, query);
+                if (rc > 0) {
+                        modbus_reply(ctx, query, rc, mb_mapping);
+                        /* mb_mapping的值在modbus_reply()之后才会更新 */
+                        hold_reg_process(modbus_config_buffer, mb_mapping);
+                }
         }
 }
 
